6c68d37dcc6d64452fc63d35f05e32e1f935048b
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-05-30 20:35:14 +0300

    Added isset to signIn2 API

--- signIn2.php
+++ signIn2.php
@@ -1,11 +1,22 @@
 <?php
 header("Access-Control-Allow-Origin:*");
 header("Access-Control-Allow-Headers: *");
-include("connection.php");
+include("./db/connection.php");
 
-$email = $_POST["email"];
-$password = hash("sha256", $_POST["password"]);
 
+// set parameters and execute
+if (isset($_POST["email"])){
+  $email= $_POST['email'];
+}else {
+  die("email, is missing");
+}
+if (isset($_POST["password"])){
+  $password = hash("sha256", $_POST["password"]);
+}else {
+  die("password, is missing");
+}
+
+// Returns iduser, id_admin OR empty array meaning user not found
 $query = $mysqli->prepare("Select iduser, is_admin from users where email = ? AND password = ?");
 $query->bind_param("ss", $email, $password);
 
