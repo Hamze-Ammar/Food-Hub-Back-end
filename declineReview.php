f576580482d5bbb135725ef015d843e51406cb4a
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-05-31 02:29:04 +0300

    Added decline review

---
+++ declineReview.php
@@ -0,0 +1,35 @@
+<?php
+header("Access-Control-Allow-Origin:*");
+header("Access-Control-Allow-Headers: *");
+include("./db/connection.php");
+
+// set parameters and execute
+if (isset($_POST["idreview"])){
+    $idreview= $_POST['idreview'];
+}else {
+    die("Id was not received");
+}
+
+
+// prepare and bind
+$query = $mysqli->prepare("DELETE FROM reviews WHERE idreview=? ");
+$query->bind_param("s", $idreview );
+
+$query->execute();
+
+//response
+$response = [];
+$response["success"] = true;
+
+echo json_encode($response);
+
+//close conx
+$query->close();
+$mysqli->close();
+
+?>
+
+
+
+
+
